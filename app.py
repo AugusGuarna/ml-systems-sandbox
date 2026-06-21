0d0bd712bf5f639b158bb65f13333b222b344e33
Augusto Guarnaccio <augustoguarnaccio@gmail.com>
2026-06-21 20:38:04 -0300

    designed_logger

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from pydantic import Field
 import joblib
 import numpy as np
 from uuid import uuid4
+import structlog
+import time
 
 # This are pydantic classes which ensure that the input and output of the API are in the correct format.
 class PredictRequest(BaseModel):
@@ -23,15 +25,26 @@ model = joblib.load('models/v1.pkl')
 def correct_shape(features: list[float]):
     return np.array(features).reshape(1, -1)
 
+# We use structlog for logging, it is a structured logging library that allows us to log in a structured way, which is useful for debugging and monitoring.
+log = structlog.get_logger()
+
 @app.get("/health")
 def health_check():
     return {"status":"ok"}
 
 @app.post("/predict", response_model=PredictResponse)
 def prediction(request: PredictRequest):
+    start_time = time.perf_counter()
     # As a prediction is the argmax over the probabilities we compute it once so we do not have to call the model twice which can be heavy
     # Think that every call to the model is a call to a heavy deep learning model that has to be lifted, we do not want to call it twice for the same input
     probabilities = model.predict_proba(correct_shape(request.features))[0]
+    # End the timer
+    end_time = time.perf_counter()
+    latency = (end_time - start_time)*1000
     predicted_value = np.argmax(probabilities)
-    return {"prediction": int(predicted_value[0]), "probability": float(probabilities.max()),
-            "model_version": "v1", "request_id": str(uuid4())}
+    probability = probabilities.max()
+    request_id = uuid4()
+    #log information in structlog logger must start with event
+    log.info(event="prediction",request_id = str(request_id), input = request.features, output = int(predicted_value), model_version = "v1", latency_ms = latency)
+    return {"prediction": int(predicted_value), "probability": float(probability),
+            "model_version": "v1", "request_id": str(request_id)}
