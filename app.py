8b74e46ceaaa7de8d14398670917543d054d4d56
Augusto Guarnaccio <augustoguarnaccio@gmail.com>
2026-06-21 21:46:44 -0300

    separated_config_from_code

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import numpy as np
 from uuid import uuid4
 import structlog
 import time
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # This are pydantic classes which ensure that the input and output of the API are in the correct format.
 class PredictRequest(BaseModel):
@@ -17,13 +18,22 @@ class PredictResponse(BaseModel):
     model_version: str
     request_id: str
 
-app = FastAPI()
-
-model = joblib.load('models/v1.pkl')
+class Settings(BaseSettings):
+    # We need this in order to read from the .env
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    # Env variables
+    model_path: str
+    api_version: str
 
 # This reshape is done as SKLearn models expect 2D array as input, even for single prediction
 def correct_shape(features: list[float]):
     return np.array(features).reshape(1, -1)
+
+settings = Settings()
+
+app = FastAPI()
+
+model = joblib.load(settings.model_path)
 
 # We use structlog for logging, it is a structured logging library that allows us to log in a structured way, which is useful for debugging and monitoring.
 log = structlog.get_logger()
@@ -44,7 +54,7 @@ def prediction(request: PredictRequest):
     predicted_value = np.argmax(probabilities)
     probability = probabilities.max()
     request_id = uuid4()
-    #log information in structlog logger must start with event
-    log.info(event="prediction",request_id = str(request_id), input = request.features, output = int(predicted_value), model_version = "v1", latency_ms = latency)
+    # Log information in structlog logger must start with event
+    log.info(event="prediction",request_id = str(request_id), input = request.features, output = int(predicted_value), model_version = settings.api_version, latency_ms = latency)
     return {"prediction": int(predicted_value), "probability": float(probability),
-            "model_version": "v1", "request_id": str(request_id)}
+            "model_version": settings.api_version, "request_id": str(request_id)}
